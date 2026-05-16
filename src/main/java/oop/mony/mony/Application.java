4561425885d3858495d531a230e66122c12838e5
Trinh Minh Quang <itsquang.vn@gmail.com>
2026-05-16 23:05:49 +0700

    fix unused imports in LoginController.java, matched app window size to FXML layout, cleaned up .gitignore, replaced password shareholder in profilePage.fxml with plain mask

--- src/main/java/oop/mony/mony/Application.java
+++ src/main/java/oop/mony/mony/Application.java
@@ -1,21 +1,19 @@
 package oop.mony.mony;
 
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.PasswordField;
 import javafx.stage.Stage;
 
-import java.awt.*;
 import java.io.IOException;
 
 public class Application extends javafx.application.Application {
-
+    private static final int WINDOW_WIDTH = 1440;
+    private static final int WINDOW_HEIGHT = 1024;
 
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader loader = new FXMLLoader(Application.class.getResource("login.fxml"));
-        Scene scene = new Scene(loader.load(), 1980, 1020);
+        Scene scene = new Scene(loader.load(), WINDOW_WIDTH, WINDOW_HEIGHT);
         stage.setTitle("Mony");
         stage.setScene(scene);
         stage.show();
